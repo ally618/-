fdc4dcf09f2253083bdc8cba621b9781225d6c14
ally618 <113585639+ally618@users.noreply.github.com>
2023-04-15 21:54:36 +0900

    Update PhoneBook.java

--- PhoneBook.java
+++ PhoneBook.java
@@ -17,32 +17,34 @@ public class PhoneBook {
 		
 		Phone[] p;
 		p= new Phone[n]; //인원수 만큼의 레퍼런스 배열 생성
+		int i;
 		
-		for(int i=0;i<p.length;i++) {
+		for( i=0;i<p.length;i++) {
 			System.out.print("이름과 전화번호(이름과 번호는 빈 칸없이 입력)>>");
 			String name=scanner.next();
 			String tel=scanner.next();
 			p[i]=new Phone(name,tel);
 		}
 		System.out.println("저장되었습니다...");
-		
+	
 		while(true) {
 			System.out.print("검색할 이름>>");
-			String SearchName=scanner.next();
+			String SearchName=scanner.next(); 
 			
 			if (SearchName.equals("그만"))
 				break;
 			
-			for(int i=0;i<p.length;i++) {
+			for(i=0;i<p.length;i++) {
 				if (SearchName.equals(p[i].name)) 
 					System.out.println(p[i].name+"의 번호는 "+p[i].tel+"입니다.");
-			
-				else
+					
+				else if (i==p.length)
 					System.out.println(SearchName+"이 없습니다.");
-			
-			}	
+			    
+				}
+		
 		}
 		scanner.close();
 	}
 }
-
+		
